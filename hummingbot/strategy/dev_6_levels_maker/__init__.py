9b5e1b4b12668f9515968dd90dbb85154781beb3
raybillbeta <raybillbeta.001@gmail.com>
2021-04-19 00:45:03 +0200

    second

--- hummingbot/strategy/dev_6_levels_maker/__init__.py
+++ hummingbot/strategy/dev_6_levels_maker/__init__.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
-from .dev_6_levels_maker import LevelsMakertrategy
+from .dev_6_levels_maker import LevelsMakerStrategy
 
 
 __all__ = [
-    LevelsMakertrategy
+    LevelsMakerStrategy
 ]
